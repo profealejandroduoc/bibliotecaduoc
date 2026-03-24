498ea6acfdc05cd0103cd0ecdbc62529617fe0f6
El profe <al.sepulvedam@profesor.duoc.cl>
2026-03-24 12:13:51 -0300

    Arreglado

--- src/main/java/com/example/bibliotecaduoc/BibliotecaduocApplication.java
+++ src/main/java/com/example/bibliotecaduoc/BibliotecaduocApplication.java
@@ -3,11 +3,15 @@ package com.example.bibliotecaduoc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.bibliotecaduoc.model.Libro;
+import com.example.bibliotecaduoc.repository.LibroRepository;
+
 @SpringBootApplication
 public class BibliotecaduocApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BibliotecaduocApplication.class, args);
+		SpringApplication.run(BibliotecaduocApplication.class, args) 
+		
 	}
 
 }
